11bdb81033b29edf11f00b8ca1d2fa75a28943d7
Joaquin Martinez <joaquinmartinezfranco@gmail.com>
2024-09-10 16:51:20 -0300

    modificacion

--- TP_Sugeno_SPY.py
+++ TP_Sugeno_SPY.py
@@ -9,9 +9,6 @@ from substractive_clustering import substractive_clustering
 from fis import *
 import datetime as dt
 
-dates = ['01/02/1991','01/03/1991','01/04/1991']
-x = [dt.datetime.strptime(d,'%m/%d/%Y').date() for d in dates]
-
 def main():
 	csvFile = pd.read_csv('spy.csv')
 	print(csvFile)
@@ -25,7 +22,6 @@ def main():
 	plt.plot(datos_x,datos_y)
 
 	plt.show()
-
 
 
 if __name__=="__main__":
