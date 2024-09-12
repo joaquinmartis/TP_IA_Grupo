f54a42000592a501cdd7cdf7a4b1f41e1890b0fa
Joaquin Martinez <joaquinmartinezfranco@gmail.com>
2024-09-12 09:21:10 -0300

    Cambios Mamdani

--- TP_Mamdani_Docentes.py
+++ TP_Mamdani_Docentes.py
@@ -2,12 +2,11 @@ import skfuzzy as fuzz
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def funcion_pertenencia_concepto_regular(concepto):
 
     match concepto:
         case "Regular":
-            return 1
+            return 0.5
         case "Bueno":
             return 0
         case "Excelente":
@@ -19,7 +18,7 @@ def funcion_pertenencia_concepto_bueno(concepto):
         case "Regular":
             return 0
         case "Bueno":
-            return 1
+            return 0.7
         case "Excelente":
             return 0
         
@@ -33,33 +32,71 @@ def funcion_pertenencia_concepto_excelente(concepto):
         case "Excelente":
             return 1
         
-def main():
+def f_pert_nota_examen_baja(nota_examen):
+    x2_vec_baja=[0,0.2,1.8,2]
+    return fuzz.trapmf(nota_examen,x2_vec_baja)
+
+def f_pert_nota_examen_media_baja(nota_examen):
+    x2_vec_media_baja=[0.8,2,3.8,4]
+    return fuzz.trapmf(nota_examen,x2_vec_media_baja)
 
 
-    x1_in=0
-    x2_in=0
-    #x1 variable Concepto
+def f_pert_nota_examen_media(nota_examen):
+    x2_vec_media=[3,6,15,20]
+    return fuzz.trapmf(nota_examen,x2_vec_media)
 
-    x1_f_pert_regular=4
-    x1_f_pert_bueno=7
-    x1_f_pert_excelente=10
+
+def f_pert_nota_examen_media_alta(nota_examen):
+    x2_vec_media_alta=[3,6,15,20]
+    return fuzz.trapmf(nota_examen,x2_vec_media_alta)
+
+
+def f_pert_nota_examen_alta(nota_examen):
+    x2_vec_alta=[3,6,15,20]
+    return fuzz.trapmf(nota_examen,x2_vec_alta)
+
+
+def f_pert_nota_examen_muy_alta(nota_examen):
+    x2_vec_muy_alta=[3,6,15,20]
+    return fuzz.trapmf(nota_examen,x2_vec_muy_alta)
+
+
+def calcula_nota_difuso(x1_in=7,x2_in="Bueno"):
+    """
+        x1: Entrada para la regla difusa relativa a la nota del examen del alumno. Rango: [0,100]
+        x2: Entrada para la regla difusa relativa al concepto del alumno: Puede tomar valor "Regular", "Bueno", "Excelente"
+
+        @return: La funcion retorna la nota final que le corresponde al alumno
+    """
+
+    x1_f_pert_regular=funcion_pertenencia_concepto_regular(x1_in)
+    x1_f_pert_bueno=funcion_pertenencia_concepto_bueno(x1_in)
+    x1_f_pert_excelente=funcion_pertenencia_concepto_excelente(x1_in)
 
 
     #x2 variable nota_examen [0,100]
-    x2_vec_baja=[-20,-15,-6,-3]
-    x2_f_pert_baja=fuzz.trapmf(x2_in,x2_vec_baja)
-    x2_vec_media_baja=[-6,-3,3,6]
-    x2_f_pert_media_baja=fuzz.trapmf(x2_in,x2_vec_media)
-    x2_vec_media=[3,6,15,20]
-    x2_f_pert_media=fuzz.trapmf(x2_in,x2_vec_alta)
-    x2_vec_media_alta=[3,6,15,20]
-    x2_f_pert_media_alta=fuzz.trapmf(x2_in,x2_vec_alta)
-    x2_vec_alta=[3,6,15,20]
-    x2_f_pert_alta=fuzz.trapmf(x2_in,x2_vec_alta)
-    x2_vec_perfecto=[3,6,15,20]
-    x2_f_pert_perfecto=fuzz.trapmf(x2_in,x2_vec_alta)
+    
+    x2_f_pert_baja=
+    
+    x2_f_pert_media_baja=
+
+    x2_f_pert_media=
+
+    x2_f_pert_media_alta=
+
+    x2_f_pert_alta=
+
+    x2_f_pert_muy_alta=
 
 
+
+    return nota_final
+
+
+
+
+
+def main():
 
 
 
