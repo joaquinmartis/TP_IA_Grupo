3e9cc78a8f4299f3f20556da27c1b5af5e9b4eb1
Joaquin Martinez <joaquinmartinezfranco@gmail.com>
2024-09-12 22:30:07 -0300

    actualizacion

--- TP_Mamdani_Docentes.py
+++ TP_Mamdani_Docentes.py
@@ -32,72 +32,91 @@ def funcion_pertenencia_concepto_excelente(concepto):
         case "Excelente":
             return 1
         
+
+
 def f_pert_nota_examen_baja(nota_examen):
-    x2_vec_baja=[0,0.2,1.8,2]
-    return fuzz.trapmf(nota_examen,x2_vec_baja)
+    centro=5
+    ancho=-2
+    return fuzz.sigmf(nota_examen,centro,ancho)    
 
 def f_pert_nota_examen_media_baja(nota_examen):
-    x2_vec_media_baja=[0.8,2,3.8,4]
-    return fuzz.trapmf(nota_examen,x2_vec_media_baja)
+    media=20
+    sigma=3
+    return fuzz.gaussmf(nota_examen,media,sigma)
 
 
 def f_pert_nota_examen_media(nota_examen):
-    x2_vec_media=[3,6,15,20]
-    return fuzz.trapmf(nota_examen,x2_vec_media)
+    media=60
+    sigma=3
+    return fuzz.gaussmf(nota_examen,media,sigma)
 
 
 def f_pert_nota_examen_media_alta(nota_examen):
-    x2_vec_media_alta=[3,6,15,20]
-    return fuzz.trapmf(nota_examen,x2_vec_media_alta)
+    media=80
+    sigma=3
+    return fuzz.gaussmf(nota_examen,media,sigma)
 
 
 def f_pert_nota_examen_alta(nota_examen):
-    x2_vec_alta=[3,6,15,20]
-    return fuzz.trapmf(nota_examen,x2_vec_alta)
-
+    media=90
+    sigma=3
+    return fuzz.gaussmf(nota_examen,media,sigma)
 
 def f_pert_nota_examen_muy_alta(nota_examen):
-    x2_vec_muy_alta=[3,6,15,20]
-    return fuzz.trapmf(nota_examen,x2_vec_muy_alta)
+    centro=95
+    ancho=2
+    return fuzz.sigmf(nota_examen,centro,ancho)
 
 
-def calcula_nota_difuso(x1_in=7,x2_in="Bueno"):
+
+def calcula_aumento_nota_difuso(nota_examen=7,x2_in="Bueno"):
     """
         x1: Entrada para la regla difusa relativa a la nota del examen del alumno. Rango: [0,100]
         x2: Entrada para la regla difusa relativa al concepto del alumno: Puede tomar valor "Regular", "Bueno", "Excelente"
 
         @return: La funcion retorna la nota final que le corresponde al alumno
     """
-
-    x1_f_pert_regular=funcion_pertenencia_concepto_regular(x1_in)
-    x1_f_pert_bueno=funcion_pertenencia_concepto_bueno(x1_in)
-    x1_f_pert_excelente=funcion_pertenencia_concepto_excelente(x1_in)
+    
+    x1_f_pert_regular=funcion_pertenencia_concepto_regular(x_in)
+    x1_f_pert_bueno=funcion_pertenencia_concepto_bueno(x_in)
+    x1_f_pert_excelente=funcion_pertenencia_concepto_excelente(x_in)
 
 
     #x2 variable nota_examen [0,100]
+    x2=np.arange(0,100,1)
+    x2_f_pert_baja=f_pert_nota_examen_baja(x2)
     
-    x2_f_pert_baja=
-    
-    x2_f_pert_media_baja=
+    x2_f_pert_media_baja=f_pert_nota_examen_media_baja(x2)
 
-    x2_f_pert_media=
+    x2_f_pert_media=f_pert_nota_examen_media(x2)
 
-    x2_f_pert_media_alta=
+    x2_f_pert_media_alta=f_pert_nota_examen_media_alta(x2)
 
-    x2_f_pert_alta=
+    x2_f_pert_alta=f_pert_nota_examen_alta(x2)
 
-    x2_f_pert_muy_alta=
+    x2_f_pert_muy_alta=f_pert_nota_examen_muy_alta(x2)
 
 
 
+
+
+
+
+    nota_final=0
     return nota_final
 
 
 
+def calcula_nota_final(nota_examen,concepto):
+    nota_final= np.round(nota_examen/10+calcula_aumento_nota_difuso(nota_examen))
 
+    #Si la nota se pasa de 10 entonces le queda un 10
+    return min(10,nota_final)
 
 def main():
-
+    nota_examen=100
+    concepto="Excelente"
+    nota_final=calcula_nota_final(nota_examen,concepto):
 
 
     pass
--- substractive_clustering.py
+++ substractive_clustering.py
@@ -88,19 +88,7 @@ def substractive_clustering(datos, Ra, Rb=0, AcceptRatio=0.3, RejectRatio=0.1):
     vec_centros = scaler.inverse_transform(vec_centros)
 
     return vector_de_pertenencia, vec_centros
-
-# c1 = np.random.rand(15,2)+[1,1]
-# c2 = np.random.rand(10,2)+[10,1.5]
-# c3 = np.random.rand(5,2)+[4.9,5.8]
-# m = np.append(c1,c2, axis=0)
-# m = np.append(m,c3, axis=0)
-
-# r,c = subclust2(m,2)
-
-# plt.figure()
-# plt.scatter(m[:,0],m[:,1])
-# plt.scatter(c[:,0],c[:,1], marker='X')
-# print(c)
+    
 if __name__=="__main__":
     centro_1 = np.random.rand(25,2)+[1,1]
     centro_2 = np.random.rand(50,2)+[10,1.5]
